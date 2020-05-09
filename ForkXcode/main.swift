d6b8aee0d9664a45d3f2e5ccb779e2aba60969f8
Lim Jun Young <nogurijr@naver.com>
2020-05-09 23:22:56 +0900

    Initial Commit

---
+++ ForkXcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ForkXcode
+//
+//  Created by #include on 2020/05/09.
+//  Copyright © 2020 Lim Jun Young. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
